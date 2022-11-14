c83176c8c15f4c54bd8f2bb253eed809eafbac15
ocobanoglu18 <ocobanoglu18@ku.edu.tr>
2022-11-14 16:56:09 +0300

    Initial Commit

---
+++ Starter/StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterApp.swift
+//  Starter
+//
+//  Created by Adnan Cobanoglu on 14.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
